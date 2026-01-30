fd76ce3f66ffe4e06ac9e85b5a9245dabacc6cf5
Atharva Jadhav <atharvajadhavlm10@gmail.com>
2026-01-30 14:21:50 +0530

    Delete backend directory

--- backend/routes/fetch_book.js
+++
@@ -1,8 +0,0 @@
-import fetchBooks from "../controllers/fetch_book.js";
-import express from "express";
-
-const router = express.Router();
-
-router.get("/fetch-books", fetchBooks);
-
-export default router;
--- backend/routes/update_book.js
+++
@@ -1,8 +0,0 @@
-import updateBook from "../controllers/update_book.js";
-import express from "express";
-
-const router = express.Router();
-
-router.put("/update-book/:id", updateBook);
-
-export default router;
